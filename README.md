883069081bd4b26a5822eca014f8f645c69ad915
FernandoFoletto <fernandovidalfoletto@gmail.com>
2025-01-04 23:22:38 -0300

    game done

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dice Game Prank
+
+I made this game as a prank for my nephew. It’s a simple dice game where two people argue, and the dice roll decides the winner. You just put in your names, and let the RNG settle things.
+
+But here’s the fun part: when my nephew enters his name (Lucca), the dice roll only goes up to 2. 😂 So, he pretty much loses every time. It’s hilarious!
+
+I built this game using some good old-fashioned vanilla CSS, JavaScript, and HTML. No fancy libraries, just pure classic web tech.
+
+Enjoy, and may the dice roll in your favor!
+
+---
+
+**Author**: Fernando Foletto
---
+++ script.js
@@ -0,0 +1,58 @@
+var player1Name;
+var player2Name;
+
+var loserName = "lucca";
+var maxLoserPoints = 2
+
+var comecarBtn = document.querySelector("#comecarBtn");
+
+comecarBtn.addEventListener("click", function() {
+    // Salva os valores dos campos de entrada nas variáveis
+    player1Name = document.querySelector("#nomeJogador1").value;
+    player2Name = document.querySelector("#nomeJogador2").value;
+
+    localStorage.setItem("player1Name", player1Name);
+    localStorage.setItem("player2Name", player2Name);
+});
+
+var player1Name = localStorage.getItem("player1Name");
+var player2Name = localStorage.getItem("player2Name");
+
+document.querySelector(".player1NameGame").textContent= "Player name: " + player1Name;
+document.querySelector(".player2NameGame").textContent= "Player name: " + player2Name;
+
+// inicio do jogo
+
+function playGame(){
+
+    // Randomizar os pontos do jogador 1
+    if (loserName === player1Name.toLowerCase()){
+        var randomNumber1 = Math.floor(Math.random() * maxLoserPoints) + 1;
+    } else{
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    }
+    
+    document.querySelector(".img1").setAttribute("src", "images/dice"+ randomNumber1 +".png") //Altera a imagem do dado 1
+
+    // Randomizar os pontos do jogador 1
+    if (loserName === player2Name.toLowerCase()){
+        var randomNumber2 = Math.floor(Math.random() * maxLoserPoints) + 1;
+    } else{
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    }
+
+    document.querySelector(".img2").setAttribute("src", "images/dice"+ randomNumber2 +".png") //Altera a imagem do dado 2
+
+
+    // Definir mensagem do vencedor
+
+    if (randomNumber1 > randomNumber2){
+        document.querySelector(".winnerMessage").innerHTML = "🚩" + player1Name +" 1 Wins!";
+    } else if (randomNumber2 > randomNumber1){
+        document.querySelector(".winnerMessage").innerHTML = player2Name + " Wins! 🚩";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+      }
+}
+
+
